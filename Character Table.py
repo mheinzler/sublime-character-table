9787a492e48999125ca1839978136c68ba00fad4
Kay-Uwe (Kiwi) Lorenz <kiwi@franka.dyndns.org>
2013-12-07 01:49:39 +0100

    added preview panel for unicode lookup

--- Character Table.py
+++ Character Table.py
@@ -1,4 +1,4 @@
-import sublime_plugin, sublime, os, sys
+import sublime_plugin, sublime, os, sys, json
 ST3 = sublime.version() >= '3000'
 
 def load_resource(name, encoding="utf-8"):
@@ -17,97 +17,24 @@ def load_binary_resource(name):
         with open(fn, "rb") as f:
             return f.read()
 
-
-class DigraphLookupCommand(sublime_plugin.WindowCommand):
-    def run(self):
-        is_digraph_spec = False
-
-        s = sublime.load_binary_resource('Packages/Character Table/README.rst').decode('utf-8')
-
-        options = []
-
-        for line in s.splitlines():
-            if not line: continue
-
-            if is_digraph_spec:
-
-                #import spdb ; spdb.start()
-                if line.startswith("==== ======") or line.startswith("char "):
-                    is_digraph_spec = False
-                    continue
-
-                try:
-                    char, digraph, hex, decimal, desc = line.split(None, 4)
-                except:
-                    continue
-
-                if line.startswith("\\"):
-                    char = line[1]
-
-                if u"MIDLINE HORIZONTAL ELLIPSIS" in desc:
-                    digraph = u". "
-
-                hex = hex.replace("0x", "00")
-
-                options.append(u"%s %s U+%s %s" % (char, digraph, hex, desc))
-
-            if line.startswith("==== ======"):
-                is_digraph_spec = True
-                continue
-
-        def on_done(index):
-            if index > -1:
-                self.window.active_view().run_command("insert", {"characters": options[index][0]})
-
-        #import spdb ; spdb.start()
-        self.window.show_quick_panel(options, on_done, sublime.MONOSPACE_FONT)
-
 class UnicodeLookupCommand(sublime_plugin.WindowCommand):
     def run(self):
-        s = load_resource('UnicodeData.txt')
-        rfc1345 = load_resource("rfc1345.txt")
-
-        mnemonics = {}
-        for line in rfc1345.splitlines():
-            if len(line) < 4: continue
-            if line[0] != " ": continue
-            if line[1] == " ": continue
-
-            mnemonic, number, iso_name = line[1:].split(None, 2)
-            if len(mnemonic) == 1:
-                mnemonic += " "
-            mnemonics[iso_name] = mnemonic
-
-        #import spdb ; spdb.start()
-
-        import csv, unicodedata
-        unicodedata_reader = csv.reader(s.splitlines(), delimiter=";", )
-
-        unicode_data = []
-        for row in unicodedata_reader:
-            iso_name = row[1]
-
-            # skip chars from "Other" Category
-            if row[2].startswith("C"): continue
-
-            try:
-                unicode_data.append(u"%s U+%s %s %s" % (
-                    unicodedata.lookup(iso_name), 
-                    row[0],
-                    mnemonics.get(iso_name, "  "),
-                    iso_name,
-                    ))
-
-            except Exception as e:
-                sys.stderr.write("Error processing %s: %s\n" % (row, e))
-                continue
-
         def on_done(index):
             if index > -1:
-                self.window.active_view().run_command("insert", {"characters": unicode_data[index][0]})
+                self.window.active_view().run_command("insert", {"characters": UNICODE_DATA[index][0]})
 
-        #import spdb ; spdb.start()
-        self.window.show_quick_panel(unicode_data, on_done, sublime.MONOSPACE_FONT)
+        preview = self.window.create_output_panel("unicode_preview")
+
+        preview.settings().set("font_size", 72)
+        self.window.run_command("show_panel", {"panel": "output.unicode_preview"})
+
+        def on_highlighted(index):
+            char = UNICODE_DATA[index][0]
+            preview.run_command("select_all")
+            preview.run_command("insert", {"characters": char})
+
+        self.window.show_quick_panel(UNICODE_DATA, on_done, 
+            sublime.MONOSPACE_FONT, -1, on_highlighted)
 
 
 
@@ -119,6 +46,7 @@ class DigraphToggleCommand(sublime_plugin.ApplicationCommand):
 
     def run(self):
         toggle_digraph(sublime.active_window().active_view())
+
 
 def toggle_digraph(view, set_state=True):
     dir = sublime.packages_path()
@@ -166,14 +94,89 @@ class DigraphListener(sublime_plugin.EventListener):
     def on_activated(self, view):
         digraph_set_status(view)
 
-if 0:
-  class DigraphInsertInInsertModeCommand(sublime_plugin.TextCommand):
+RFC1345_MNEMONICS = {}
+UNICODE_DATA = []
 
-    def run(self, edit, characters=""):
-        settings = view.settings()
-        if 'Vintage' not in settings.get('ignored_packages'):
-            if not settings.get('command_mode'):
-                return
+def load_character_table():
+    s = load_resource('UnicodeData.txt')
+    rfc1345 = load_resource("rfc1345.txt")
 
-        self.view.run_command('insert', {'characters': characters})
+    mnemonics = RFC1345_MNEMONICS
+    mnemonics.clear()
+    for line in rfc1345.splitlines():
+        if len(line) < 4: continue
+        if line[0] != " ": continue
+        if line[1] == " ": continue
 
+        mnemonic, number, iso_name = line[1:].split(None, 2)
+        if len(mnemonic) == 1:
+            mnemonic += " "
+        try:
+            char = eval( ('u"\\U%08s"'%number).replace(" ", "0") )
+        except:
+            sys.stderr.write("Error decoding: %s, %s\n" % (number, iso_name))
+        mnemonics[char] = mnemonic
+
+    import csv, unicodedata
+    unicodedata_reader = csv.reader(s.splitlines(), delimiter=";", )
+
+    unicode_data = UNICODE_DATA
+    unicode_data[:] = []
+    for row in unicodedata_reader:
+        iso_name = row[1]
+
+        # skip chars from "Other" Category
+        if row[2].startswith("C"): continue
+
+        try:
+            char = eval( ('u"\\U%08s"'%row[0]).replace(" ", "0") )
+            unicode_data.append(u"%s U+%s %s %s" % (
+                char,
+                row[0],
+                mnemonics.get(char, "  "),
+                iso_name,
+                ))
+
+        except Exception as e:
+            sys.stderr.write("Error processing %s: %s\n" % (row, e))
+            continue
+
+ADD_REVERSED_MNEMONICS = True
+
+def plugin_loaded():
+    load_character_table()
+    user_dir = os.path.join(sublime.packages_path(), 'User', "Character Table", "Default")
+
+    if not os.path.exists(user_dir):
+        os.makedirs(user_dir)
+
+    def makekeydef(chars, unichr):
+        return {
+            "keys": ["ctrl+k"]+[x for x in chars],
+            "command": "insert",
+            "args": {"characters": unichr},
+        }
+
+    keymap = []
+
+    import unicodedata, re
+
+    mnemonics = set(RFC1345_MNEMONICS.values())
+
+    for k,v in RFC1345_MNEMONICS.items():
+
+        if ADD_REVERSED_MNEMONICS:
+            if len(v) == 2:
+                rev = "".join([ x for x in reversed(v)])
+                if rev[0] != " " and rev not in mnemonics:
+                    keymap.append(makekeydef(rev, k))
+
+        keymap.append(makekeydef(v, k))
+
+    fn = os.path.join(user_dir, "Default.sublime-keymap")
+    with open(fn+'.tmp', "w") as f:
+        json.dump(keymap, f, indent=4, ensure_ascii=False)
+    os.rename(fn+'.tmp', fn)
+
+if not ST3:
+    plugin_loaded()
